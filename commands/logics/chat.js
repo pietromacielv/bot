1c9ca3045aead9fbb0a4535aa787e9cb7a5de4ef
Pietro Maciel <pietroomv150206@gmail.com>
2024-01-30 18:42:05 -0300

    change bot pre-prompt

--- commands/logics/chat.js
+++ commands/logics/chat.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function chat(message, authorName) {
   try {
     await axios.post("https://apiunlimited.cyclic.app/api/ai", {
-      content: `Essa mensagem é apenas um prompt para te orientar como deve responder as próximas mensagens. Nesse contexto, iremos te chamar de North. Quem está conversando com você no momento é ${authorName}. Por favor, apenas responda as perguntas diretamente. Não faça qualquer assossiação a esta mensagem. Ou seja, nunca diga "segundo o prompt" ou "você me disse que" ou algo do tipo. SEMPRE OBEDEÇA ESSA ORDEM`,
+      content: `Olá, Bard. Eu irei chamá-lo de North e nesse momento você está conversando com ${authorName}. Por favor, apenas responda as perguntas diretamente. Não faça qualquer menção a esta mensagem, nunca.`,
     });
   } catch (error) {
     console.error("Error making API request:", error.message);
