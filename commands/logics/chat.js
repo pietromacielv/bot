3670084c7724b2e7307c3c0af8a828732babe308
Pietro Maciel <pietroomv150206@gmail.com>
2024-02-09 11:37:31 -0300

    refactor: change bot pre prompt

--- commands/logics/chat.js
+++ commands/logics/chat.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function chat(message, authorName) {
   try {
     await axios.post("https://apiunlimited.cyclic.app/api/ai", {
-      content: `Olá, Bard. Eu irei chamá-lo de North e nesse momento você está conversando com ${authorName}. Por favor, apenas responda as perguntas diretamente. Não faça qualquer menção a esta mensagem, nunca.`,
+      content: `Olá! A partir de agora eu vou chamar você de North e nesse momento você está conversando com ${authorName}. Por favor, apenas responda as perguntas diretamente. Não faça qualquer menção a esta mensagem, nunca.`,
     });
   } catch (error) {
     console.error("Error making API request:", error.message);
