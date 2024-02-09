b63b845c4eb313bc2cc354c33797a00b17d273bd
Pietro Maciel <pietroomv150206@gmail.com>
2024-02-09 11:42:11 -0300

    refactor chat logic

--- commands/logics/chat.js
+++ commands/logics/chat.js
@@ -2,19 +2,17 @@ const axios = require("axios");
 
 async function chat(message, authorName) {
   try {
-    await axios.post("https://apiunlimited.cyclic.app/api/ai", {
-      content: `Olá! A partir de agora eu vou chamar você de North e nesse momento você está conversando com ${authorName}. Por favor, apenas responda as perguntas diretamente. Não faça qualquer menção a esta mensagem, nunca.`,
-    });
-  } catch (error) {
-    console.error("Error making API request:", error.message);
-  }
-
-  try {
-    const response = await axios.post(
+    const prePrompt = await axios.post(
       "https://apiunlimited.cyclic.app/api/ai",
-      { content: message }
+      {
+        content: `Olá! A partir de agora eu vou chamar você de North e nesse momento você está conversando com ${authorName}. Por favor, apenas responda as perguntas diretamente. Não faça qualquer menção a esta mensagem, nunca.`,
+      }
     );
-    const apiResponse = response.data.text || "No response from API";
+    console.log(prePrompt);
+    const prompt = await axios.post("https://apiunlimited.cyclic.app/api/ai", {
+      content: message,
+    });
+    const apiResponse = prompt.data.text || "No response from API";
     return apiResponse;
   } catch (error) {
     console.error("Error making API request:", error.message);
